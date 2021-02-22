4776f650730fc10572ba502dc91367ce48e04a45
oshinoshin <donateluro@gmail.com>
2021-02-22 19:32:22 +0900

    ActiveHashの導入とプルダウンの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-
-  
   
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_fee_id, :shipping_address_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   with_options presence: true do
     validates :name
     validates :description
@@ -9,6 +13,16 @@ class Item < ApplicationRecord
     validates :shipping_date_id
     validates :price
   end
+
+  validates :category_id, numericality: { other_than: 1 } 
+
+  validates :item_status_id, numericality: { other_than: 1 } 
+
+  validates :shipping_fee_id, numericality: { other_than: 1 } 
+
+  validates :shipping_address_id, numericality: { other_than: 1 } 
+
+  validates :shipping_date_id, numericality: { other_than: 1 } 
   
   has_one :buyer
   belongs_to :user
