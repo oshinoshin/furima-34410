d504bab2c93c0f5b9814b474cdeafa88aaa78ea7
oshinoshin <donateluro@gmail.com>
2021-03-03 18:23:12 +0900

    クレジットカードでの購入機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,12 +1,15 @@
 class BuyersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
+      pay_item
       @buyer_address.save
       redirect_to root_path
     else
@@ -17,7 +20,16 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_address).permit(:postal_code, :shipping_address_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buyer_address).permit(:postal_code, :shipping_address_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_5aa2bbe534e6f6d28981471b"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
